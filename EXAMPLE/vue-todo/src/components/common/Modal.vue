cc97dcd11e6e6fdd9e347e91015ae3657665d7d7
Dongjoon Lee <djunnni@gmail.com>
2022-11-09 09:11:50 +0900

    [EXAMPLE]: vue-todo

---
+++ EXAMPLE/vue-todo/src/components/common/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="modal" v-if="showModal" @close="showModal == false">
+        <h3 slot="header">경고</h3>
+        <span slot="footer" @click="closeModal">
+            할 일을 입력하세요.
+            <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Modal",
+    props:['showModal'],
+    methods: {
+        closeModal() {
+            this.$emit('close-modal');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.modal {
+    background-color: #FFF;
+}
+</style>
