bf89c224d6dbc89b1d0f228930ea432c9916ac9e
Dongjoon Lee <djunnni@gmail.com>
2022-12-08 17:45:09 +0900

    monorepo example

---
+++ monorepo/readme.md
@@ -0,0 +1,4 @@
+## 모노레포란? 
+
+monolithic + repository => mono + repo (단단히 하나로 되어있는 git 저장소)
+
---
+++ monorepo/yarn-1-workspace/packages/server/index.js
@@ -0,0 +1,8 @@
+const foo = require("@study/common");
+const bar = require("@study/lib");
+
+console.log("hello from server");
+
+foo();
+
+bar();
